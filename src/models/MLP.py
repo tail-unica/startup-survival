d8fdd71b6228543c640afab3667ef276b4dbbd7f
GiulioCasti <95642656+GiulioCasti@users.noreply.github.com>
2026-03-19 11:01:44 +0100

    MLP custom

--- src/models/MLP.py
+++ src/models/MLP.py
@@ -1,26 +1,33 @@
+import torch
 import torch.nn as nn
 
 class MLP(nn.Module):
-    def __init__(self, input_size, hidden_sizes, output_size):
+    def __init__(self, input_size, hidden_sizes,dropout_rate=0.3, batch_norm=True):
         super(MLP, self).__init__()
-        
-        # Se viene passato un intero singolo, trasformazione in lista
+
         if isinstance(hidden_sizes, int):
             hidden_sizes = [hidden_sizes]
-            
+
         self.layers = nn.ModuleList()
-        
+
+        # Primo layer
         self.layers.append(nn.Linear(input_size, hidden_sizes[0]))
-        self.layers.append(nn.ReLU())
-        
-        # Layer intermedi (Hidden -> Hidden)
+        if batch_norm:
+            self.layers.append(nn.BatchNorm1d(hidden_sizes[0]))
+        self.layers.append(nn.Tanh())
+        self.layers.append(nn.Dropout(dropout_rate))
+
+        # Layer intermedi
         for i in range(len(hidden_sizes) - 1):
             self.layers.append(nn.Linear(hidden_sizes[i], hidden_sizes[i+1]))
-            self.layers.append(nn.ReLU())
-            
-        # Ultimo layer (Ultimo Hidden -> Output)
-        self.layers.append(nn.Linear(hidden_sizes[-1], output_size))
-        
+            if batch_norm:
+                self.layers.append(nn.BatchNorm1d(hidden_sizes[i+1]))
+            self.layers.append(nn.Tanh())
+            self.layers.append(nn.Dropout(dropout_rate))
+
+        # Output layer — 1 neurone per classificazione binaria (logit, no sigmoid)
+        self.layers.append(nn.Linear(hidden_sizes[-1], 1))
+
     def forward(self, x):
         for layer in self.layers:
             x = layer(x)
